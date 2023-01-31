2b30a9053ccafa3b11d1399ae61a4bf6aa822acc
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2023-01-31 21:49:35 +0300

    feat: container

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
+import { Container } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Chat } from './pages/chat/Chat';
 import { Footer } from './pages/footer/Footer';
 import { Header } from './pages/header/Header';
-import { Page404 } from './pages/page404/Page404';
 import { Main } from './pages/main/Main';
-import { Chat } from './pages/chat/Chat';
+import { Page404 } from './pages/page404/Page404';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/chat" element={<Chat />} />
-        {/* <Route path="/anyPage" element={< AnyPage/>} /> */}
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-      <Footer />
+      <Container sx={{ width: { sm: 2 / 3 } }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/chat" element={<Chat />} />
+          {/* <Route path="/anyPage" element={< AnyPage/>} /> */}
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+        <Footer />
+      </Container>
     </BrowserRouter>
   );
 }
